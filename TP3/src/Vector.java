bf43c924ead1612d4f1fef14676198fdc2f564f2
bejenaruruslan <ruslan.bejenaru@u-psud.fr>
2021-09-27 17:18:51 +0200

    Exo 1 du tp3

---
+++ TP3/src/Vector.java
@@ -0,0 +1,31 @@
+public class Vector {
+    private double x;
+    private double y;
+    private double z;
+
+    public Vector(double a, double b, double c){
+        this.x = a;
+        this.y = b;
+        this.z = c;
+    }
+
+    public String toString() {
+
+        return "( " + this.x + ", " + this.y + ", " + this.z + " )";
+    }
+
+    public double norm() {
+
+        return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
+    }
+
+    public Vector add(Vector v) {
+
+        return new Vector(this.x + v.x, this.y + v.y, this.z + v.z);
+    }
+
+    public double dot(Vector v) {
+
+        return this.x * v.x + this.y * v.y + this.z * v.z;
+    }
+}
